0e41b351d0227223c9ed599b4774cba2334cc29d
Ma Long <malongcq@gmail.com>
2013-06-05 17:51:07 +0800

    modified:   map/nxgviz.py

--- map/nxgviz.py
+++ map/nxgviz.py
@@ -4,19 +4,20 @@ import argparse
 import ast
 
 import networkx as nx
-import matplotlib.pyplot as plt
 
-def do_main(fn_nxg, w=32, h=18, dpi=80, links=[]):
-    gg = nx.read_gpickle(fn_nxg)
-    src = gg.graph['Source'] if 'Source' in gg.graph else ''
-    print 'G={nodes=%d, links=%d, source=%s, srid=%s, points=%d}' % (len(gg.nodes()),len(gg.edges()),src,gg.graph['SRID'],len(gg.graph['Points']))
+def do_draw(G, fn_nxg, w=32, h=18, dpi=80, links=[]):
+    from matplotlib import pyplot as plt
+    import numpy as np
     
+    Start = datetime.datetime.now()
     plt.figure(figsize=(w,h),dpi=dpi)
     
-    for (u,v,d) in gg.edges_iter(data=True):
-        xys = [gg.graph['Points'][p] for p in d['points']]
-        xs = [x for x,y in xys]
-        ys = [y for x,y in xys]
+    for (u,v,d) in G.edges_iter(data=True):
+        xys = [G.graph['Points'][p] for p in d['points']]
+        #xs = [x for x,y in xys]
+        #ys = [y for x,y in xys]
+        xs = np.array([x for x,y in xys])
+        ys = np.array([y for x,y in xys])
         
         if links != None and d['id'] in links:
             plt.plot(xs, ys, 'r', linewidth=2)
@@ -24,7 +25,79 @@ def do_main(fn_nxg, w=32, h=18, dpi=80, links=[]):
             plt.plot(xs, ys, 'b', linewidth=1)
           
     plt.savefig('%s.png'%(fn_nxg))
+    print 'Finished img in %fs' %(datetime.datetime.now() - Start).total_seconds()
+    
     plt.show()
+
+def do_draw2(G, width=800, height=600, links=[]):
+    from Tkinter import *
+    
+    root = Tk()
+    canvas = Canvas(root,width=800,height=600,scrollregion=(0,0,width,height))
+    hbar = Scrollbar(canvas,orient=HORIZONTAL)
+    hbar.pack(side=BOTTOM,fill=X)
+    hbar.config(command=canvas.xview)
+    vbar = Scrollbar(canvas,orient=VERTICAL)
+    vbar.pack(side=RIGHT,fill=Y)
+    vbar.config(command=canvas.yview)
+    canvas.config(width=800,height=600)
+    canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+    canvas.pack(side=LEFT,expand=True,fill=BOTH)
+    
+    xx0,xx1,yy0,yy1 = 345000,390000,135000,165000
+    w2w, h2h = width/float(xx1-xx0), height/float(yy1-yy0)
+    
+    for (u,v,d) in G.edges_iter(data=True):
+        clr = 'red' if links != None and d['id'] in links else 'blue'
+        
+        xys = [((x-xx0)*w2w,(yy1-y)*h2h) for x,y in [G.graph['Points'][p] for p in d['points']]]
+        x0,y0 = xys[0]
+        for x1,y1 in xys[1:]:
+            canvas.create_line(x0, y0, x1, y1, fill=clr)
+            x0,y0 = x1,y1
+            
+    root.mainloop()
+
+def do_draw3(G, bbox, links=[]):
+    import pyglet
+
+    xx0,xx1,yy0,yy1 = bbox
+    
+    window = pyglet.window.Window(resizable=True,width=800,height=600)
+    pyglet.gl.glClearColor(1, 1, 1, 1)
+    
+    @window.event
+    def on_draw():
+        window.clear()
+        w2w, h2h = window.width/float(xx1-xx0), window.height/float(yy1-yy0)
+
+        pyglet.gl.glColor4f(0,0,1,1)
+        #batch = pyglet.graphics.Batch()    
+        for (u,v,d) in G.edges_iter(data=True):
+            xys = [((x-xx0)*w2w,(y-yy0)*h2h) for x,y in [G.graph['Points'][p] for p in d['points']]]
+            pyglet.graphics.draw(len(xys), pyglet.gl.GL_LINES,('v2f', sum(xys,())))
+            
+    pyglet.app.run()
+
+def do_main(fn_nxg, w=32, h=18, dpi=80, links=[]):
+    gg = nx.read_gpickle(fn_nxg)
+    src = gg.graph['Source'] if 'Source' in gg.graph else ''
+    print 'G={nodes=%d, links=%d, source=%s, srid=%s, points=%d}' % (len(gg.nodes()),len(gg.edges()),src,gg.graph['SRID'],len(gg.graph['Points']))
+
+    l = nx.get_edge_attributes(gg,'points').values()
+    ps = [item for sublist in l for item in sublist]
+    xs = [gg.graph['Points'][p][0] for p in ps]
+    ys = [gg.graph['Points'][p][1] for p in ps]
+    
+    import math
+    xx0,xx1 = math.floor(min(xs)*0.99), math.ceil(max(xs)*1.01)
+    yy0,yy1 = math.floor(min(ys)*0.99), math.ceil(max(ys)*1.01)
+    print min(xs),max(xs),min(ys),max(ys)
+    print xx0,xx1,yy0,yy1
+    
+    #do_draw(gg,fn_nxg,w,h,dpi,links)
+    #do_draw2(gg,width=w*dpi,height=h*dpi,links=links)
+    do_draw3(gg,(xx0,xx1,yy0,yy1),links=links)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
